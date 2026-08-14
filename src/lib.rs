ac11ae336d20d3181fb098d9e6bc66a4c4b67b99
Cole Helbling <cole.e.helbling@outlook.com>
2026-08-14 07:09:30 -0700

    Reuse pkg_config v0.3.34's Library::extract_lib_from_filename

--- src/lib.rs
+++ src/lib.rs
@@ -561,7 +561,9 @@ impl Dependencies {
 
                         // Try to extract the library linking name from the filename, e.g.
                         // libboost_context.a -> boost_context
-                        if let Some(lib_name) = extract_lib_from_filename(target, &filename) {
+                        if let Some(lib_name) =
+                            pkg_config::Library::extract_lib_from_filename(target, &filename)
+                        {
                             if new {
                                 flags.add(BuildFlag::SearchNative(
                                     dir.to_string_lossy().to_string(),
@@ -1567,70 +1569,5 @@ impl fmt::Display for ParseError {
         match self {
             Self::VariantNotFound(v) => write!(f, "Unknown variant: `{v}`"),
         }
-    }
-}
-
-// Copied from pkg-config-rs:
-// https://github.com/rust-lang/pkg-config-rs/blob/057321c21329ead3ec7d502a8e730a5fd7a271e9/src/lib.rs#L807
-#[expect(clippy::needless_return, reason = "copied as-is from pkg-config")]
-#[expect(clippy::manual_strip, reason = "copied as-is from pkg-config")]
-/// Extract the &str to pass to cargo:rustc-link-lib from a filename (just the file name, not including directories)
-/// using target-specific logic.
-fn extract_lib_from_filename<'a>(target: &str, filename: &'a str) -> Option<&'a str> {
-    fn test_suffixes<'b>(filename: &'b str, suffixes: &[&str]) -> Option<&'b str> {
-        for suffix in suffixes {
-            if filename.ends_with(suffix) {
-                return Some(&filename[..filename.len() - suffix.len()]);
-            }
-        }
-        None
-    }
-
-    let prefix = "lib";
-    if target.contains("windows") {
-        if target.contains("gnu") && filename.starts_with(prefix) {
-            // GNU targets for Windows, including gnullvm, use `LinkerFlavor::Gcc` internally in rustc,
-            // which tells rustc to use the GNU linker. rustc does not prepend/append to the string it
-            // receives via the -l command line argument before passing it to the linker:
-            // https://github.com/rust-lang/rust/blob/657f246812ab2684e3c3954b1c77f98fd59e0b21/compiler/rustc_codegen_ssa/src/back/linker.rs#L446
-            // https://github.com/rust-lang/rust/blob/657f246812ab2684e3c3954b1c77f98fd59e0b21/compiler/rustc_codegen_ssa/src/back/linker.rs#L457
-            // GNU ld can work with more types of files than just the .lib files that MSVC's link.exe needs.
-            // GNU ld will prepend the `lib` prefix to the filename if necessary, so it is okay to remove
-            // the `lib` prefix from the filename. The `.a` suffix *requires* the `lib` prefix.
-            // https://sourceware.org/binutils/docs-2.39/ld.html#index-direct-linking-to-a-dll
-            let filename = &filename[prefix.len()..];
-            return test_suffixes(filename, &[".dll.a", ".dll", ".lib", ".a"]);
-        } else {
-            // According to link.exe documentation:
-            // https://learn.microsoft.com/en-us/cpp/build/reference/link-input-files?view=msvc-170
-            //
-            //   LINK doesn't use file extensions to make assumptions about the contents of a file.
-            //   Instead, LINK examines each input file to determine what kind of file it is.
-            //
-            // However, rustc appends `.lib` to the string it receives from the -l command line argument,
-            // which it receives from Cargo via cargo:rustc-link-lib:
-            // https://github.com/rust-lang/rust/blob/657f246812ab2684e3c3954b1c77f98fd59e0b21/compiler/rustc_codegen_ssa/src/back/linker.rs#L828
-            // https://github.com/rust-lang/rust/blob/657f246812ab2684e3c3954b1c77f98fd59e0b21/compiler/rustc_codegen_ssa/src/back/linker.rs#L843
-            // So the only file extension that works for MSVC targets is `.lib`
-            // However, for externally created libraries, there's no
-            // guarantee that the extension is ".lib" so we need to
-            // consider all options.
-            // See:
-            // https://github.com/mesonbuild/meson/issues/8153
-            // https://github.com/rust-lang/rust/issues/114013
-            return test_suffixes(filename, &[".dll.a", ".dll", ".lib", ".a"]);
-        }
-    } else if target.contains("apple") {
-        if filename.starts_with(prefix) {
-            let filename = &filename[prefix.len()..];
-            return test_suffixes(filename, &[".a", ".so", ".dylib"]);
-        }
-        return None;
-    } else {
-        if filename.starts_with(prefix) {
-            let filename = &filename[prefix.len()..];
-            return test_suffixes(filename, &[".a", ".so"]);
-        }
-        return None;
     }
 }
